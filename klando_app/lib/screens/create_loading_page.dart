4de02d67766a24261b1b64364da7b6e463fffcaf
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-02-14 20:32:03 +0300

    finished coding the loading screen for create account

--- klando_app/lib/screens/create_loading_page.dart
+++ klando_app/lib/screens/create_loading_page.dart
@@ -16,13 +16,47 @@ class CreateLoadingWidget extends StatelessWidget {
       ));
     });
     return Scaffold(
+      backgroundColor: Colors.grey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            CircularProgressIndicator(),
-            SizedBox(height: 16),
-            Text(loadingMessage ?? 'Please wait...'),
+            //CircularProgressIndicator(),
+            //SizedBox(height: 16),
+            Container(
+              width: 300,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Creating Account!!!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Please Wait...',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //Text(loadingMessage ?? 'Please wait...'),
           ],
         ),
       ),
