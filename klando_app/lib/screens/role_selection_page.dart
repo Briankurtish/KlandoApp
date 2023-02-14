79e5396cfb8cdb43562b246e4e9a36e8705e5e1b
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-02-14 18:29:13 +0300

    created Role selection page

---
+++ klando_app/lib/screens/role_selection_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoleSelectionPage extends StatefulWidget {
+  const RoleSelectionPage({super.key});
+
+  @override
+  State<RoleSelectionPage> createState() => _RoleSelectionPageState();
+}
+
+class _RoleSelectionPageState extends State<RoleSelectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
